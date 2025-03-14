34aaec3cbf69090f9bfba7eebf746c12f7d7f8c5
setha <sethasith123@gmail.com>
2025-03-15 06:12:21 +0700

    exercise

--- Array/array.cpp
+++ Array/array.cpp
@@ -44,114 +44,6 @@
 
 // exercise : 
 
-// Create for storing stock product
-// 1. Create
-// 2. Display
-// 3. Payment to Riel
-// 4. Payment to Dollar
-
-// #include <iostream>
-// using namespace std;
-// int code[20],qty[20],n;
-// string name[20];
-// float price[20],total;
-// void createProduct(){
-//     cout<<"Enter number of products: ";
-//     cin>>n;
-//     for(int i = 0; i < n; i++){
-//         cout<<"Enter product code: ";
-//         cin>>code[i];
-//         cout<<"Enter product name: ";
-//         cin.ignore();
-//         getline(cin,name[i]);
-//         cout<<"Enter product price: ";
-//         cin>>price[i];
-//         cout<<"Enter product quantity: ";
-//         cin>>qty[i];
-//     }
-// }
-
-// void displayProduct(){
-//     cout<<"================================================"<<endl;
-//     cout<<"Code\tProduct Name\tPrice\tQuantity"<<endl;
-//     cout<<"================================================"<<endl;
-//     for(int i = 0; i < n; i++){
-//         cout<<code[i]<<"\t"<<name[i]<<"\t"<<price[i]<<"\t"<<qty[i]<<endl;
-//     }
-//     cout<<"================================================"<<endl;
-// }
-
-// void paymentRiel(){
-//     cout<<"================================================"<<endl;
-//     cout<<"Code\tProduct Name\tPrice\tQuantity\tTotal Price"<<endl;
-//     cout<<"================================================"<<endl;
-//     for(int i = 0; i < n; i++){
-//         total = price[i] * qty[i];
-//         cout<<code[i]<<"\t"<<name[i]<<"\t"<<price[i]<<"\t"<<qty[i]<<"\t"<<total<<endl;
-//     }
-//     cout<<"================================================"<<endl;
-//     cout<<"Total Price in Riel: "<<total*4000<<endl;
-//     cout<<"================================================"<<endl;
-
-
-// }
-
-// void paymentDollar(){
-
-//     cout<<"================================================"<<endl;
-//     cout<<"Code\tProduct Name\tPrice\tQuantity\tTotal Price"<<endl;
-//     cout<<"================================================"<<endl;
-//     for(int i = 0; i < n; i++){
-//         total = price[i] * qty[i];
-//         cout<<code[i]<<"\t"<<name[i]<<"\t"<<price[i]<<"\t"<<qty[i]<<"\t"<<total<<endl;
-//     }
-//     cout<<"================================================"<<endl;
-//     cout<<"Total Price in Dollar: "<<total*0.00034<<endl;
-//     cout<<"================================================"<<endl;
-
-// }
-// int main(){
-//     int option;
-//     do{
-//         cout<<"================================================"<<endl;
-//         cout<<"1. Create Product"<<endl;
-//         cout<<"2. Display Product"<<endl;
-//         cout<<"3. Payment to Riel"<<endl;
-//         cout<<"4. Payment to Dollar"<<endl;
-//         cout<<"0. Exit"<<endl;
-//         cout<<"================================================"<<endl;
-//         cout<<"Enter your choice: ";
-//         cin>>option;
-//         switch(option){
-//             case 0:{
-//                 cout<<"================================================"<<endl;
-//                 cout<<"\tThank you for using our program"<<endl;
-//                 cout<<"================================================"<<endl;
-//                 break;
-//             }
-//             case 1 :{
-//                 createProduct();
-//                 break;
-//             }
-//             case 2 :{
-//                 displayProduct();
-//                 break;
-//             }
-//             case 3 :{
-//                 paymentRiel();
-//                 break;
-//             }
-//             case 4 :{
-//                 paymentDollar();
-//                 break;
-//             }
-//             default:
-//                 cout<<"Invalid Option. Please try again."<<endl;
-//                 break;
-//         }
-
-//     }while(option != 0);
-// // }
 // #include<iostream>
 // #include<iomanip>
 // #include<windows.h>
@@ -278,110 +170,213 @@
 // for (i=0;i<size;i++){
 // cout<<"Array ["<<i<<"] = "<<arr[i]<<endl;
 // }
+// #include<iostream>
+// using namespace std;
+
+//     int op,size,search;
+//     int k,arr[100];
+//     bool check;
+
+// int main(){
+
+// 	system("cls");
+//     do{
+//         cout<<"\t\t===============Menu Option============="<<endl;
+//         cout<<"\t\t1.Input Value in Array.\n";
+//         cout<<"\t\t2.Display Value in Array."<<endl;
+//         cout<<"\t\t3.Search Value"<<endl;
+//         cout<<"\t\t4.Delete Value"<<endl;
+//         cout<<"\t\t5.Update Value"<<endl;
+//         cout<<"\t\t6.Exit[0]"<<endl;
+// 		cout<<"\t\tPlease choose an option : ";cin>>op;
+// 		switch(op){
+// 			case 0: {
+
+// 				cout<<"\t\t====================ByeBye========================\n";
+//                 break;
+// 			}
+// 			case 1 : {
+// 				cout<<"\t\t====================Input========================\n";
+// 				cout<<"Enter array size: ";cin>>size;            
+//                 for(int i=0;i<size;i++) {
+// 					cout<<"Enter elements: ";cin>>arr[i];
+// 				}
+//                 break;
+// 			}
+// 			case 2 :{
+// 				cout<<"\t\t====================Display========================\n";
+//                 for(int i=0;i<size;i++) {
+//                     cout<<"Elements: "<<arr[i]<<endl;
+//                 }
+//                 break;
+// 			}
+// 			case 3 : {
+// 				cout<<"\t\t====================Search========================\n";
+//                 cout<<"Enter the element to search: ";cin>>search;
+//                 check=false;
+//                 for(int i=0;i<size;i++) {
+//                     if(arr[i]==search) {
+//                         check=true;
+//                         cout<<"Element found at position: "<<i+1<<endl;
+//                         break;
+//                     }
+//                 }
+//                 if(!check) cout<<"Element not found.\n";
+//                 break;
+// 			}
+// 			case 4: {
+// 				int Delete;
+// 				cout<<"\t\t====================[Delete]===================="<<endl;
+// 				cout<<"Enter the position to delete: ";cin>>Delete;
+// 				check=true;
+// 				for (int i=0;i<size;i++){
+// 					if(Delete == arr[i] ){
+// 						for(int j=0;j<size - 1;j++){
+// 							arr[j] = arr[j +1];
+// 						}
+// 						size--;
+// 					}
+// 					cout<<"Element Delete successfully!!!"<<endl;
+// 					check = false;
+// 					break;
+// 				}
+// 				if(check){
+// 					cout<<"Element not found.\n";
+// 				}
+				
+// 				break;
+// 			}
+// 			case 5:{
+// 				cout<<"\t\t======================Update========================\n";
+// 				int update;
+// 				cout<<"Enter the position to update: ";cin>>update;
+// 				check = true;
+// 				for(int i=0;i<size;i++){
+// 					if(update == arr[i]){
+// 						int newValue;
+// 						cout<<"Enter the new value: ";cin>>newValue;
+// 						arr[i] = newValue;
+// 						check = false;
+// 						cout<<"Element Update successfully!!!"<<endl;
+//                         break;
+// 					}
+// 				}
+// 				if(check){
+//                     cout<<"Element not found.\n";
+//                 }
+//                 break;
+// 			}
+// 			default: {
+//                 cout<<"\t\t====================Invalid Option========================\n";
+//                 break;
+//             }
+// 		}
+        
+//     }while(op!=0);
+    
+
+//     return 0;
+// }
+
+// Create program about phone - code name price quantity
+// 1. Create for storing phone
+// 2. Display stock phone
+// 3. Payment to Riel
+// 4. Payment to Dollar
+
+
+
 #include<iostream>
 using namespace std;
+int code[20],quantity[20],option,size;
+string name[20];
+float price[20];
+double total,payment;
+void CreatePhone(){
+	int j=0;
+	cout<<"=================================================="<<endl;
+	cout<<"º\t Create a new phone"<<endl;
+	cout<<"=================================================="<<endl;
+	cout<<"Enter size of phone to create : ";cin>>size;
+	while(j<size){
+		cout<<"\nEnter phone code : ";cin>>code[j];
+		cout<<"Enter phone name : ";cin>>name[j];
+		cout<<"Enter phone price : ";cin>>price[j];
+		cout<<"Enter phone quantity : ";cin>>quantity[j];
+		j++;
+	}
+	cout<<"=================================================="<<endl;
+	cout<<"\tPhone created successfully!"<<endl;
+	cout<<"=================================================="<<endl;
+}
 
-    int op,size,search;
-    int k,arr[100];
-    bool check;
+void DisplayPhone(){
+	int j=0;
+	cout<<"=================================================="<<endl;
+	cout<<"º\t Display all phone"<<endl;
+	cout<<"=================================================="<<endl;
+	cout<<"Code Phone \t Name Phone \t Price Phone \t Quantity Phone "<<endl;
+	while(j<size){
+		cout<<code[j]<<"\t\t"<<name[j]<<"\t\t"<<price[j]<<"\t\t"<<quantity[j]<<endl;
+        j++;
+	}
+	cout<<"=================================================="<<endl;
+	cout<<"\t\tEnd Display phone"<<endl;
+	cout<<"=================================================="<<endl;
+
+}
+
+void PaymentRiel(){
+	float riel = 4000;
+	cout<<"=================================================="<<endl;
+    cout<<"º\t Payment in Riel"<<endl;
+    cout<<"=================================================="<<endl;
+	for (int i=0;i<size;i++){
+		total += price[i] * quantity[i];
+	}
+	// vivo = 200 * 1000 = 200,000
+	// oppo = 150 * 1000 = 150,000
+	// total = 200,000 + 150,000;
+	payment = total * riel;
+	cout<<"Total price in Riel : "<<fixed<<payment<<" Riel"<<endl;
+    cout<<"=================================================="<<endl;
+    cout<<"=================================================="<<endl;
+}
 
 int main(){
-
 	system("cls");
-    do{
-        cout<<"\t\t===============Menu Option============="<<endl;
-        cout<<"\t\t1.Input Value in Array.\n";
-        cout<<"\t\t2.Display Value in Array."<<endl;
-        cout<<"\t\t3.Search Value"<<endl;
-        cout<<"\t\t4.Delete Value"<<endl;
-        cout<<"\t\t5.Update Value"<<endl;
-        cout<<"\t\t6.Exit[0]"<<endl;
-		cout<<"\t\tPlease choose an option : ";cin>>op;
-		switch(op){
-			case 0: {
-
-				cout<<"\t\t====================ByeBye========================\n";
+	do{
+		cout<<"\t\t\t Store Phone"<<endl;
+		cout<<"1.Create Phone "<<endl;
+		cout<<"2.Display Phone"<<endl;
+		cout<<"3.Payment in Riel"<<endl;
+		cout<<"4.Payment in Dollar"<<endl;
+		cout<<"0.Exit"<<endl;
+		cout<<"Enter your option : ";cin>>option;
+		switch(option){
+			case 0 :{
+				cout<<"================================================="<<endl;
+				cout<<"º\t\t\tThank you!"<<endl;
+				cout<<"================================================="<<endl;
                 break;
 			}
 			case 1 : {
-				cout<<"\t\t====================Input========================\n";
-				cout<<"Enter array size: ";cin>>size;            
-                for(int i=0;i<size;i++) {
-					cout<<"Enter elements: ";cin>>arr[i];
-				}
-                break;
-			}
-			case 2 :{
-				cout<<"\t\t====================Display========================\n";
-                for(int i=0;i<size;i++) {
-                    cout<<"Elements: "<<arr[i]<<endl;
-                }
-                break;
-			}
-			case 3 : {
-				cout<<"\t\t====================Search========================\n";
-                cout<<"Enter the element to search: ";cin>>search;
-                check=false;
-                for(int i=0;i<size;i++) {
-                    if(arr[i]==search) {
-                        check=true;
-                        cout<<"Element found at position: "<<i+1<<endl;
-                        break;
-                    }
-                }
-                if(!check) cout<<"Element not found.\n";
-                break;
-			}
-			case 4: {
-				int Delete;
-				cout<<"\t\t====================[Delete]===================="<<endl;
-				cout<<"Enter the position to delete: ";cin>>Delete;
-				check=true;
-				for (int i=0;i<size;i++){
-					if(Delete == arr[i] ){
-						for(int j=0;j<size - 1;j++){
-							arr[j] = arr[j +1];
-						}
-						size--;
-					}
-					cout<<"Element Delete successfully!!!"<<endl;
-					check = false;
-					break;
-				}
-				if(check){
-					cout<<"Element not found.\n";
-				}
-				
+				// function create or input 
+				CreatePhone();
+
 				break;
 			}
-			case 5:{
-				cout<<"\t\t======================Update========================\n";
-				int update;
-				cout<<"Enter the position to update: ";cin>>update;
-				check = true;
-				for(int i=0;i<size;i++){
-					if(update == arr[i]){
-						int newValue;
-						cout<<"Enter the new value: ";cin>>newValue;
-						arr[i] = newValue;
-						check = false;
-						cout<<"Element Update successfully!!!"<<endl;
-                        break;
-					}
-				}
-				if(check){
-                    cout<<"Element not found.\n";
-                }
-                break;
+			case 2 : {
+				DisplayPhone();
+				break;
 			}
-			default: {
-                cout<<"\t\t====================Invalid Option========================\n";
+			case 3 : {
+                PaymentRiel();
                 break;
             }
-		}
-        
-    }while(op!=0);
-    
+		}	
 
-    return 0;
+
+	}while(option != 0);
+	
 }
